838bc9cd3684eeb0fdad1c9cdcdc4ecc240e4b28
Kelvin Okonkwo <kelly2000s@yahoo.com>
2024-11-30 20:01:52 +0100

    Interceptors

---
+++ LiquorSalesMicroservices/Services/Odering/Odering.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odering.Infrastructure.Data.Interceptors
+{
+    internal class AuditableEntityInterceptor
+    {
+    }
+}
